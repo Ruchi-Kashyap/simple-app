37ff692d922e66905efbc6ed4715e34a5f14ff6a
Ruchi-Kashyap <ruchikashyap2001@gmail.com>
2026-04-30 21:28:30 +0530

    Fixed metrics endpoint

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,9 +1,27 @@
-const request = require("supertest");
-const app = require("../app");
+const http = require("http");
+const client = require("prom-client");
 
-describe("GET /", () => {
-  it("should return hello message", async () => {
-    const res = await request(app).get("/");
-    expect(res.text).toBe("Hello !!!!!!!!!! this is a sample app ");
-  });
+// collect default metrics
+client.collectDefaultMetrics();
+
+const server = http.createServer(async (req, res) => {
+  
+  // 👉 Metrics endpoint
+  if (req.url === "/metrics") {
+    res.writeHead(200, { "Content-Type": client.register.contentType });
+    res.end(await client.register.metrics());
+    return;
+  }
+
+  // 👉 Default route
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello !!!!!!!!!! this is a sample app with metrics 🚀");
 });
+
+if (require.main === module) {
+  server.listen(3000, () => {
+    console.log("Server running on port 3000");
+  });
+}
+
+module.exports = server;
